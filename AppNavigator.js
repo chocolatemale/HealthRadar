665cd5454e0127e4f12f1224e49b7511f61db176
Vincent Liew <vincent@ManchingsAirM1.lan>
2023-08-08 12:46:15 -0700

    Added bottom tab navigator

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,19 +1,53 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomePage from './screens/HomePage';
-import ViewCaloriesRecord from './screens/ViewCaloriesRecord'; // Add this import
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHome, faClock, faUtensils, faUser } from '@fortawesome/free-solid-svg-icons';
 
-const Stack = createStackNavigator();
+import HomePage from './screens/HomePage';
+import ViewCaloriesRecord from './screens/ViewCaloriesRecord';
+
+const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Daily Details" component={ViewCaloriesRecord} />
-        {/* Additional screens will be added here */}
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Overview') {
+              iconName = focused ? faHome : faHome;
+            } else if (route.name === 'Today') {
+              iconName = focused ? faClock : faClock;
+            } else if (route.name === 'Food') {
+              iconName = focused ? faUtensils : faUtensils;
+            } else if (route.name === 'Profile') {
+              iconName = focused ? faUser : faUser;
+            }
+
+            return <FontAwesomeIcon icon={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#3498db',
+          inactiveTintColor: 'gray',
+          labelStyle: {
+            fontSize: 12,
+            fontWeight: 'bold',
+          },
+          style: {
+            backgroundColor: '#f4f4f4', // You can set the background color according to your theme
+          },
+        }}
+      >
+        <Tab.Screen name="Overview" component={HomePage} />
+        <Tab.Screen name="Today" component={ViewCaloriesRecord} />
+        {/* You can add other screens here for Food and Profile */}
+        <Tab.Screen name="Food" component={() => <></>} />
+        <Tab.Screen name="Profile" component={() => <></>} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
